46c0066a772a08ec2a5b33f0c33b4f3a9f0b1cb5
Ron <rieck@landaumedia.de>
2020-05-14 17:54:48 +0200

    minor changes in infrastructure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-require('dotenv').config();
-const Discord = require('discord.js')
-const Client = new Discord.Client();
-const token = process.env.Client();
+require("dotenv").config();
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-Client.login(token);
+bot.commands = new Discord.Collection();
+const botCommands = require("./commands");
 
-Client.once('ready', () => {
-    console.log('ready')
-})
+bot.login(process.env.TOKEN);
 
+bot.once("ready", () => {
+  console.log("ready");
+});
+
+Object.keys(botCommands).map((key) => {
+  bot.commands.set(botCommands[key].name, botCommands[key]);
+});
+
+bot.on("message", (msg) => {
+  const args = msg.content.split(/ +/);
+  const command = args.shift().toLocaleLowerCase();
+
+  console.info("called command:", command);
+
+  if (!bot.commands.has(command)) return;
+
+  try {
+    bot.commands.get(command).execute(msg, args);
+    console.log("executed ping");
+  } catch (error) {
+    console.error("something went wrong... check the command import");
+    msg.reply("ACHTUNG: das Command wurde nicht gefunden oder existiert nicht");
+  }
+});
